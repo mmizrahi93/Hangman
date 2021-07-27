039ecc98e912309fd040f753b123a62e64b4ab1a
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-07-27 11:02:14 -0400

    started guess letter function and added splitword

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ let randomWord = 'hangman'
 
 let guessedLetters = [];
 let incorrectGuessedLetters = []
-let notGuessedYetLetters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+let notGuessedYetLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let wrongGuesses = 6
+let splitWord = randomWord.split('')
 
 
+const guessLetter = () => {
+    if(notGuessedYetLetters === )
+}
 
 
